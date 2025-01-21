754ac49780e5a7de4bb6d80fb41f24810c32caf3
Chen Xu <xckomorebi@icloud.com>
2025-01-21 13:26:48 +0800

    (#5) implemented code action provider

---
+++ src/codeActionProvider.ts
@@ -0,0 +1,23 @@
+import * as vscode from "vscode";
+
+export const golangCodeActionProvider = {
+    provideCodeActions(): vscode.ProviderResult<vscode.CodeAction[]> {
+        return [
+            {
+                title: 'Generate getter and setter',
+                kind: vscode.CodeActionKind.Source,
+                command: { command: 'go.sourceAction.accessor', title: 'Generate getter and setter' },
+            },
+            {
+                title: 'Generate constructor',
+                kind: vscode.CodeActionKind.Source,
+                command: { command: 'go.sourceAction.constructor', title: 'Generate constructor' },
+            },
+            {
+                title: 'Generate interface stub',
+                kind: vscode.CodeActionKind.Source,
+                command: { command: 'go.sourceAction.interfaceStub', title: 'Generate interface stub' },
+            },
+        ];
+    }
+};
